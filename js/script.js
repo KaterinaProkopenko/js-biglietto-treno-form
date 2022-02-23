6aadbec77570f612bd75a5d0d15bdf34263d6972
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-23 22:15:03 +0100

    Aggiornamento (calcoli)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 document.querySelector("button").addEventListener('click', function(){
-let nameSurname = document.getElementById('name-surname').value;
-console.log(nameSurname);
+  let nameSurname = document.getElementById('name-surname').value;
+  console.log(nameSurname);
+  let kilometer = document.getElementById('kilometers').value;
+  console.log(kilometer);
+  document.getElementById('name-passenger').innerHTML = nameSurname;
+  let fasciaEta = document.getElementById('my-select').value;
+  console.log(fasciaEta);
+  let priceKm = 0.27;
 
-let kilometer = document.getElementById('kilometers').value;
-console.log(kilometer);
+  let priceGeneral = kilometer * priceKm;
+  let priceMinorenni = priceGeneral - ((priceGeneral * 17) / 100);
+  let priceOverSessanta = priceGeneral - ((priceGeneral * 33) / 100);
 
-let age = 
-
+  /* console.log(fasciaEta == 'Over 60'); */
+  if (fasciaEta == 'Over 60') {
+    let priceOverSessanta = priceGeneral - ((priceGeneral * 33) / 100);
+    console.log(priceOverSessanta);
+    document.getElementById('price').innerHTML = priceOverSessanta.toFixed(2) + ' Euro';
+  } else if (fasciaEta == 'Maggiorenne') {
+      let priceGeneral = kilometer * priceKm;
+      console.log(priceGeneral);
+      document.getElementById('price').innerHTML = priceGeneral.toFixed(2) + ' Euro';
+  } else if (fasciaEta == 'Minorenni') {
+      let priceMinorenni = priceGeneral - ((priceGeneral * 17) / 100);
+      console.log(priceMinorenni);
+  }
 });
-
-/* let priceKm = 0.27;
-
-let priceGeneral = kilometer * priceKm;
-
-let priceMinorenni = priceGeneral - ((priceGeneral * 17) / 100);
-
-let priceOverSessanta = priceGeneral - ((priceGeneral * 33) / 100); */
