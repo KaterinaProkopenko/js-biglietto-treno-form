d0351825ddf38a54ae135ad2e4c6f0e6b42e1486
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-23 17:24:03 +0100

    Mini aggiornamento

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.querySelector("button").addEventListener('click', function(){
+let nameSurname = document.getElementById('name-surname').value;
+console.log(nameSurname);
+
+let kilometer = document.getElementById('kilometers').value;
+console.log(kilometer);
+
+let age = 
+
+});
+
+/* let priceKm = 0.27;
+
+let priceGeneral = kilometer * priceKm;
+
+let priceMinorenni = priceGeneral - ((priceGeneral * 17) / 100);
+
+let priceOverSessanta = priceGeneral - ((priceGeneral * 33) / 100); */
